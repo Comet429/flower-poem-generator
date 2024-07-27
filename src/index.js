2dfca224ef5b603b1ad3d123a8d8743f872cbea1
Jessie P. <ritime275@gmail.com>
2024-07-27 03:25:33 -0400

    Added Typewriter plugin and basic form heading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // alert("hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "poem answer",
+    autoStart: true,
+    delay: 1,
+    cursor: "🏵",
+  });
+}
+
+let createPoemElement = document.querySelector("#create-poem");
+createPoemElement.addEventListener("submit", generatePoem);
